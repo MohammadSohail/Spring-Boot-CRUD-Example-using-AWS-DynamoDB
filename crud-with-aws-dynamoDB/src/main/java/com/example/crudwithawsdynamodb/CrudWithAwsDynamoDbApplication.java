2063326408e7c1983c674fef917711dde0a63023
sohail-inf <sohail.inflack@bkash.com>
2022-05-11 16:44:16 +0600

    init

---
+++ crud-with-aws-dynamoDB/src/main/java/com/example/crudwithawsdynamodb/CrudWithAwsDynamoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudwithawsdynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithAwsDynamoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWithAwsDynamoDbApplication.class, args);
+	}
+
+}
---
+++ crud-with-aws-dynamoDB/src/test/java/com/example/crudwithawsdynamodb/CrudWithAwsDynamoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudwithawsdynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWithAwsDynamoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
